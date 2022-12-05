ccff32181cdc441dcad95fdf1436187c138d54ba
LittleLebowski <o.f.atas@gmail.com>
2022-12-05 18:44:56 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard titles={title} genres={genresArr} posterSrc={posterURL} />
     </div>
   );
 }
